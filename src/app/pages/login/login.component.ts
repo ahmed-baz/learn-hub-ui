72fda8a24f8f315ddab7fa7267b9d0bc96a1b6ae
Ahmed Baz <ahmismail.c@stc.com.sa>
2024-11-09 19:55:52 +0200

    - update login - create token service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {LoginRequest} from '../../services/models/login-request';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../../services/services/authentication.service';
+import {LoginResponse} from '../../services/models/login-response';
+import {TokenService} from '../../services/token/token.service';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +11,14 @@ import {AuthenticationService} from '../../services/services/authentication.serv
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginRequest: LoginRequest = {email: '', password: ''};
+  loginRequest: LoginRequest = {email: 'ali@tesla.com', password: 'P@ssw0rd'};
+  loginResponse: LoginResponse = {id: 0, email: '', role: '', accessToken: ''};
   errorsMessages: Array<string> = [];
 
   constructor(
     private router: Router,
     private authService: AuthenticationService,
+    private tokenService: TokenService,
   ) {
   }
 
@@ -24,11 +28,15 @@ export class LoginComponent {
       .login({body: this.loginRequest})
       .subscribe({
         next: (res) => {
-          console.log("GET BODY")
-          // save the token
-
-
-          //this.router.navigate(['/courses']);
+          if (200 == res.statusCode) {
+            // save the token
+            this.loginResponse = res.data as LoginResponse;
+            this.tokenService.setToken(this.loginResponse.accessToken);
+            //this.router.navigate(['/courses']);
+          } else {
+            //TODO map errors
+            var validationErrors = res.validationErrors;
+          }
         },
         error: (error) => {
           console.log(error)
---
+++ src/app/services/token/token.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+}
