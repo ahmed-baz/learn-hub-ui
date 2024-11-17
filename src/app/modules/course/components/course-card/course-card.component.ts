8e966acb2aa0d61b9060c8236edffd8c754f64d0
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-11-17 14:28:24 +0200

    Implement the course card component

---
+++ src/app/modules/course/components/course-card/course-card.component.ts
@@ -0,0 +1,65 @@
+import {Component, Input} from '@angular/core';
+import {Course} from '../../../../services/models/course';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+
+  private _course: Course = new Course();
+  private _management: boolean = false;
+
+  get course(): Course {
+    return this._course;
+  }
+
+  @Input()
+  set course(course: Course) {
+    this._course = course;
+  }
+
+  get courseCover(): string | undefined {
+    if (this._course.courseImage && this._course.courseImage.data) {
+      return `data:${this._course.courseImage.type};base64,${this._course.courseImage.data}`;
+    }
+    return 'https://source.unsplash.com/user/c_v_r/1900x800';
+  }
+
+  /*
+  @Output() private share: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private showDetails: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private subscribe: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private unsubscribe: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private addToFavorites: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private edit: EventEmitter<any> = new EventEmitter<any>();
+   */
+
+  get management(): boolean {
+    return this._management;
+  }
+
+  @Input()
+  set management(value: boolean) {
+    this._management = value;
+  }
+
+  onShowDetails() {
+  }
+
+  onSubscribe() {
+  }
+
+  onAddToFavorites() {
+  }
+
+  onEdit() {
+  }
+
+  onShare() {
+  }
+
+  onUnsubscribe() {
+  }
+}
--- src/app/modules/course/course.module.ts
+++ src/app/modules/course/course.module.ts
@@ -5,13 +5,15 @@ import { CourseRoutingModule } from './course-routing.module';
 import { MainComponent } from './pages/main/main.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { CourseListComponent } from './pages/course-list/course-list.component';
+import { CourseCardComponent } from './components/course-card/course-card.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
     MenuComponent,
-    CourseListComponent
+    CourseListComponent,
+    CourseCardComponent
   ],
   imports: [
     CommonModule,
